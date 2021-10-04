4d6ee26d1e945d852c136f44412618f854c1c66d
Timmith Dysinski <Timmith@hotmail.com>
2021-10-04 03:25:29 -0400

    added height adjustment to goal and penalty line visuals

--- client/src/helpers/scenes/TestGraphics3D.ts
+++ client/src/helpers/scenes/TestGraphics3D.ts
@@ -1,5 +1,5 @@
 import { Vec2 } from "box2d";
-import { Mesh, MeshBasicMaterial, MeshStandardMaterial, Plane, Vector3, WebGLRenderer } from "three";
+import { Mesh, MeshBasicMaterial, MeshStandardMaterial, Object3D, Plane, Vector3, WebGLRenderer } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import device from "~/device";
 import TestGraphicsPack from "~/helpers/scenes/TestGraphicsPack";
@@ -15,6 +15,8 @@ export default class TestGraphics3D extends TestLightingScene {
 	b2World: Testb2World;
 	graphicsPack: TestGraphicsPack;
 	useB2Preview = getUrlFlag("debugPhysics");
+	heightGoal: Object3D | undefined;
+	dangerZone: Object3D | undefined;
 
 	constructor() {
 		super(false, false);
@@ -46,6 +48,22 @@ export default class TestGraphics3D extends TestLightingScene {
 					easing: Easing.Quartic.InOut,
 					duration: 2000
 				});
+				if (this.heightGoal) {
+					simpleTweener.to({
+						target: this.heightGoal.position,
+						propertyGoals: { y: this.heightGoal.position.y + 1 },
+						easing: Easing.Quartic.InOut,
+						duration: 2000
+					});
+				}
+				if (this.dangerZone) {
+					simpleTweener.to({
+						target: this.dangerZone.position,
+						propertyGoals: { y: this.dangerZone.position.y + 1 },
+						easing: Easing.Quartic.InOut,
+						duration: 2000
+					});
+				}
 			},
 			() => {
 				simpleTweener.to({
@@ -54,6 +72,22 @@ export default class TestGraphics3D extends TestLightingScene {
 					easing: Easing.Quartic.InOut,
 					duration: 2000
 				});
+				if (this.heightGoal) {
+					simpleTweener.to({
+						target: this.heightGoal.position,
+						propertyGoals: { y: -0.25 },
+						easing: Easing.Quartic.InOut,
+						duration: 2000
+					});
+				}
+				if (this.dangerZone) {
+					simpleTweener.to({
+						target: this.dangerZone.position,
+						propertyGoals: { y: -1.55 },
+						easing: Easing.Quartic.InOut,
+						duration: 2000
+					});
+				}
 			}
 		);
 		this.b2World.autoClear = false;
@@ -72,28 +106,28 @@ export default class TestGraphics3D extends TestLightingScene {
 				});
 			}
 			const stage = gltf.scene;
-			const heightGoal = stage.getObjectByName("heightGoal");
-			const dangerZone = stage.getObjectByName("dangerZone");
+			this.heightGoal = stage.getObjectByName("heightGoal");
+			this.dangerZone = stage.getObjectByName("dangerZone");
 			stage.scale.multiplyScalar(0.1);
 			stage.rotation.y += Math.PI;
 			stage.position.y -= 1.5;
 			this.scene.add(stage);
-			if (heightGoal) {
-				heightGoal.scale.multiplyScalar(2);
-				this.scene.attach(heightGoal);
-				heightGoal.castShadow = false;
-				heightGoal.receiveShadow = false;
-				heightGoal.position.y += 1;
+			if (this.heightGoal) {
+				this.heightGoal.scale.multiplyScalar(2);
+				this.scene.attach(this.heightGoal);
+				this.heightGoal.castShadow = false;
+				this.heightGoal.receiveShadow = false;
+				this.heightGoal.position.y = -0.25;
 			}
-			if (dangerZone) {
-				dangerZone.scale.multiplyScalar(2);
-				this.scene.attach(dangerZone);
-				dangerZone.position.y -= 0.15;
-				if (dangerZone instanceof Mesh && dangerZone.material instanceof MeshStandardMaterial) {
-					dangerZone.castShadow = false;
-					dangerZone.receiveShadow = false;
-					const map = dangerZone.material.map;
-					dangerZone.material = new MeshBasicMaterial({ map, transparent: true, opacity: 0.5 });
+			if (this.dangerZone) {
+				this.dangerZone.scale.multiplyScalar(2);
+				this.scene.attach(this.dangerZone);
+				this.dangerZone.position.y = -1.55;
+				if (this.dangerZone instanceof Mesh && this.dangerZone.material instanceof MeshStandardMaterial) {
+					this.dangerZone.castShadow = false;
+					this.dangerZone.receiveShadow = false;
+					const map = this.dangerZone.material.map;
+					this.dangerZone.material = new MeshBasicMaterial({ map, transparent: true, opacity: 0.5 });
 				}
 				// dangerZone.position.y += 1
 			}
