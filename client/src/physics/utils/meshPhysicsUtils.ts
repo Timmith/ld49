e2790a28cfc9ebcb3b0c8f6dc4ccfcc18578e8c4
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2021-10-16 23:57:34 -0700

    added caching to expensive mesh-to-fixture algorithm

--- client/src/physics/utils/meshPhysicsUtils.ts
+++ client/src/physics/utils/meshPhysicsUtils.ts
@@ -6,6 +6,8 @@ import { GLTF_MESH_SCALE } from "~/settings/constants";
 import { findCollider } from "./meshColliderUtils";
 import { makeBitMask, PBits } from "./physicsUtils";
 
+const __colliderLoopPositionsCache: Map<string, Vec2[][]> = new Map();
+
 export async function getArchitectMeshAndFixtures(
 	body: Body,
 	meshName: string,
@@ -21,9 +23,72 @@ export async function getArchitectMeshAndFixtures(
 		categoryArray,
 		maskArray
 	});
-	const collider = findCollider(mesh, colliderName);
-	const vertsAttribute = collider.geometry.attributes.position;
-	const indicesAttribute = collider.geometry.getIndex();
+	const key = `${meshName}:${colliderName}`;
+	if (!__colliderLoopPositionsCache.has(key)) {
+		const collider = findCollider(mesh, colliderName);
+		const vertsAttribute = collider.geometry.attributes.position;
+		const indicesAttribute = collider.geometry.getIndex();
+		if (vertsAttribute instanceof BufferAttribute && indicesAttribute instanceof BufferAttribute) {
+			const verts = vertsAttribute.array;
+			const indices = indicesAttribute.array;
+			const edges = new Set<string>();
+			const loops: number[][] = [];
+			for (let i3 = 0; i3 < indices.length; i3 += 3) {
+				const a = indices[i3];
+				const b = indices[i3 + 1];
+				const c = indices[i3 + 2];
+				const ab = a + ":" + b;
+				const bc = b + ":" + c;
+				const ca = c + ":" + a;
+				const ac = a + ":" + c;
+				const ba = b + ":" + a;
+				const cb = c + ":" + b;
+				if (edges.has(ba)) {
+					edges.delete(ba);
+				} else {
+					edges.add(ab);
+				}
+				if (edges.has(cb)) {
+					edges.delete(cb);
+				} else {
+					edges.add(bc);
+				}
+				if (edges.has(ac)) {
+					edges.delete(ac);
+				} else {
+					edges.add(ca);
+				}
+			}
+			const edgeMap = new Map<number, number>();
+			for (const edge of edges) {
+				const edgeData = edge.split(":").map(v => parseInt(v, 10));
+				edgeMap.set(edgeData[0], edgeData[1]);
+			}
+			while (edgeMap.size > 0) {
+				const start = edgeMap.keys().next().value;
+				let cursor = edgeMap.get(start)!;
+				const loop: number[] = [cursor];
+				edgeMap.delete(start);
+				do {
+					const val = edgeMap.get(cursor)!;
+					edgeMap.delete(cursor);
+					loop.push(val);
+					cursor = val;
+				} while (cursor !== start);
+				loops.push(loop);
+			}
+			__colliderLoopPositionsCache.set(
+				key,
+				loops.map(loop => {
+					return loop.map(i => {
+						const i3 = i * 3;
+						return new Vec2(verts[i3] * GLTF_MESH_SCALE, verts[i3 + 2] * -GLTF_MESH_SCALE);
+					});
+				})
+			);
+		}
+	}
+
 	const fixtureDef = new FixtureDef();
 
 	if (categoryArray && maskArray) {
@@ -32,65 +97,13 @@ export async function getArchitectMeshAndFixtures(
 	}
 
 	fixtureDef.density = 1;
-	if (vertsAttribute instanceof BufferAttribute && indicesAttribute instanceof BufferAttribute) {
-		const verts = vertsAttribute.array;
-		const indices = indicesAttribute.array;
+
+	for (const positions of __colliderLoopPositionsCache.get(key)!) {
 		const shape = new PolygonShape();
-		const edges = new Set<string>();
 		fixtureDef.shape = shape;
-		for (let i3 = 0; i3 < indices.length; i3 += 3) {
-			const a = indices[i3];
-			const b = indices[i3 + 1];
-			const c = indices[i3 + 2];
-			const ab = a + ":" + b;
-			const bc = b + ":" + c;
-			const ca = c + ":" + a;
-			const ac = a + ":" + c;
-			const ba = b + ":" + a;
-			const cb = c + ":" + b;
-			if (edges.has(ba)) {
-				edges.delete(ba);
-			} else {
-				edges.add(ab);
-			}
-			if (edges.has(cb)) {
-				edges.delete(cb);
-			} else {
-				edges.add(bc);
-			}
-			if (edges.has(ac)) {
-				edges.delete(ac);
-			} else {
-				edges.add(ca);
-			}
-		}
-		const edgeMap = new Map<number, number>();
-		for (const edge of edges) {
-			const edgeData = edge.split(":").map(v => parseInt(v, 10));
-			edgeMap.set(edgeData[0], edgeData[1]);
-		}
-		const loops: number[][] = [];
-		while (edgeMap.size > 0) {
-			const start = edgeMap.keys().next().value;
-			let cursor = edgeMap.get(start)!;
-			const loop: number[] = [cursor];
-			edgeMap.delete(start);
-			do {
-				const val = edgeMap.get(cursor)!;
-				edgeMap.delete(cursor);
-				loop.push(val);
-				cursor = val;
-			} while (cursor !== start);
-			loops.push(loop);
-		}
-		for (const loop of loops) {
-			const positions = loop.map(i => {
-				const i3 = i * 3;
-				return new Vec2(verts[i3] * GLTF_MESH_SCALE, verts[i3 + 2] * -GLTF_MESH_SCALE);
-			});
-			shape.Set(positions);
-			body.CreateFixture(fixtureDef);
-		}
+
+		shape.Set(positions);
+		body.CreateFixture(fixtureDef);
 	}
 	return mesh;
 }
